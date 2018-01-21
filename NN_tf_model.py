ba70ac87212b270e18cade41b18dd7e8af6dc798
jcrousse <jcrousse@gmail.com>
2018-01-21 18:08:07 +0100

    finalised model processing.

--- NN_tf_model.py
+++ NN_tf_model.py
@@ -8,9 +8,10 @@ data = pickle.load(open("data.p", "rb"))
 data_dict = pickle.load(open("data_dict.p", "rb"))
 
 # NN training parameters. We use default logging, dropout prob and CV split
-epochs = 10
+epochs = 1000
 learning_rate = 1e-3
 layers = [10, 10]  # Number of units per layer
+model_name = 'model_180121'
 
 # prepare dataset. Y= target, X = features only.
 m = data.shape[0]
@@ -20,13 +21,26 @@ X = data[data_dict['features']].values
 
 
 # myModel = NNModel(nn_2_layers)
-#myModel = myModel.train(X, Y, epochs=epochs, model_id='model_1', layers=layers, learning_rate=learning_rate)
+# myModel = myModel.train(X, Y, epochs=epochs, model_id='model_1', layers=layers, learning_rate=learning_rate)
 myModel = NNModelTTS(graph_creator=nn_2_layers)
-myModel = myModel.set_hyperparameters(n_features=X.shape[1],  epochs=epochs, model_id='model_1', layers=layers, learning_rate=learning_rate).train(X,Y)
-y = myModel.predict(X, model_id='model_1')
+myModel = myModel\
+    .set_hyperparameters(n_features=X.shape[1],  epochs=epochs, model_id=model_name,
+                         layers=layers, learning_rate=learning_rate)\
+    .train(X, Y)\
 
-prediction_binary = pd.DataFrame((np.round_(1/(1+np.exp(-y)))).astype(int), index=data.index, columns=["prediction"])
+predictions = myModel.predict(X)
+indices = myModel.get_train_test_indices()
 
-result_df = pd.concat([data, prediction_binary], axis=1)
+# re-organise data for d3 visualisaion
+
+predictions.columns = ['binary_pred', 'logit_pred']
+indices.columns = ['train_set_flg']
+
+predictions['date'] = data.index
+indices['date'] = data.index
+predictions = predictions.set_index('date')
+indices = indices.set_index('date')
+
+result_df = pd.concat([data, predictions, indices], axis=1)
 
 print(result_df)
--- nn_model.py
+++ nn_model.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 import time
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 
-#todo: Split "set hyperparameters" and "train"
-#todo figure out how to return train/test indices
+
 class NNModel:
     def __init__(self, **kwargs):  # graph is a function to generate a NN graph
         self.graph_creator = kwargs.get('graph_creator')
@@ -17,16 +17,16 @@ class NNModel:
         self.model_id = self.epochs = self.logging = self.dropout_keep = None
 
     def set_hyperparameters(self, **kwargs):
-        if 'model_id' not in  kwargs:
+        if 'model_id' not in kwargs:
             raise TypeError("missing argument model_id in function set_hyperparameters")
-        if 'layers' not in  kwargs:
+        if 'layers' not in kwargs:
             raise TypeError("missing argument layers in function set_hyperparameters")
-        if 'n_features' not in  kwargs:
+        if 'n_features' not in kwargs:
             raise TypeError("missing argument n_features in function set_hyperparameters")
 
         learning_rate = kwargs.get('learning_rate', 1e-3)
         self.x, self.y_, self.loss, self.train_step, self.keep_prob, self.y_logits \
-            = self.graph_creator(self.graph,kwargs.get('n_features'),  kwargs.get('layers'), learning_rate)
+            = self.graph_creator(self.graph, kwargs.get('n_features'),  kwargs.get('layers'), learning_rate)
 
         self.model_id = 'model_id'
         self.epochs = kwargs.get('epochs', 1000)
@@ -36,12 +36,7 @@ class NNModel:
         return self
 
     def train(self, x, y):
-
-        train_set, test_set, train_label, test_label = train_test_split(x, y, test_size=self.test_size)
-        self.training(train_set, test_set, train_label, test_label)
-        print('Training finished')
-
-        return self
+        pass
 
     def training(self,  train_set, test_set, train_label, test_label):
         err_log = []
@@ -78,11 +73,31 @@ class NNModel:
     def generate_model_path(self):
         return 'model/model{0}.ckpt'.format(self.model_id)
 
-    def predict(self, x, model_id):
+    def predict(self, x):
         with tf.Session(graph=self.graph) as sess:
             saver = tf.train.Saver()
             saver.restore(sess, self.generate_model_path())
-            return sess.run(self.y_logits, feed_dict={self.x: x, self.keep_prob: 1.0})
+            y = sess.run(self.y_logits, feed_dict={self.x: x, self.keep_prob: 1.0}).flatten()
+
+            col_log_name = 'logit_pred_' + self.generate_model_path()
+            col_bin_name = 'binary_pred_' + self.generate_model_path()
+
+            preds = {col_log_name: y, col_bin_name: (np.round_(1 / (1 + np.exp(-y)))).astype(int)}
+            return pd.DataFrame(preds)
+
+    def get_train_test_indices(self):
+        pass
+
+    def train_test_indices_df(self, train_index, test_index):
+        # return dataframe with value 1 for train index, 0 for test
+
+        array = np.sort(np.concatenate([train_index, test_index]))
+        index = pd.Index(array)
+        col_name = self.generate_model_path() + '_train_flg'
+        train_test_df = pd.DataFrame(0, index, columns=[col_name])
+        train_index = pd.Index(train_index)
+        train_test_df.loc[train_index, [col_name]] = 1
+        return train_test_df
 
 
 class NNModelKF(NNModel):
@@ -90,6 +105,9 @@ class NNModelKF(NNModel):
         super(NNModelKF, self).__init__(**kwargs)
         self.id_kf = 0  # id for currently active fold
         self.n_splits_cv = 5
+        # indices:
+        self.train_index = [[]]
+        self.test_index = [[]]
 
     def set_hyperparameters(self, **kwargs):
         self.n_splits_cv = kwargs.get('n_splits_cv', 5)
@@ -104,6 +122,8 @@ class NNModelKF(NNModel):
         Y = np.array(y)
 
         for id_kf, (train_index, test_index) in enumerate(kf.split(X)):
+            self.train_index.append(train_index)
+            self.test_index.append(test_index)
             train_set, test_set = X[train_index], X[test_index]
             train_label, test_label = Y[train_index], Y[test_index]
             self.id_kf = id_kf
@@ -118,7 +138,10 @@ class NNModelKF(NNModel):
         self.id_kf = id_kf
 
     def generate_model_path(self):
-        return 'model/model{0}_{1}.ckpt'.format(self.model_id, self.id_kf) # now contains fold ID as well
+        return 'model/model{0}_{1}.ckpt'.format(self.model_id, self.id_kf)  # now contains fold ID as well
+
+    def get_train_test_indices(self):
+        return self.train_test_indices_df(self.train_index[self.id_kf], self.test_index[self.id_kf])
 
 
 class NNModelTTS(NNModel):
@@ -126,8 +149,27 @@ class NNModelTTS(NNModel):
     def __init__(self, **kwargs):
         super(NNModelTTS, self).__init__(**kwargs)
         self.test_size = 0.2
+        # indices:
+        self.train_index = self.test_index = None
 
     def set_hyperparameters(self, **kwargs):
         self.test_size = kwargs.get('test_size', 0.2)
         super(NNModelTTS, self).set_hyperparameters(**kwargs)
+
         return self
+
+    def train(self, x, y):
+
+        seq_index = np.arange(x.shape[0])
+
+        train_index, test_index = train_test_split(seq_index, test_size=self.test_size)
+        self.training(x[train_index], x[test_index], y[train_index], y[test_index])
+        print('Training finished')
+
+        self.train_index = train_index
+        self.test_index = test_index
+
+        return self
+
+    def get_train_test_indices(self):
+        return self.train_test_indices_df(self.train_index, self.test_index)
